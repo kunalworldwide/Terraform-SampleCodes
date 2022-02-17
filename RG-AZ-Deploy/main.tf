f23b21215c6baea009e209cc0fe8ff1937948448
Kunal Das <kunalworldwide@gmail.com>
2022-02-17 20:05:05 +0530

    resource group creation

---
+++ RG-AZ-Deploy/main.tf
@@ -0,0 +1,22 @@
+terraform {
+
+  required_version = ">=0.12"
+  
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+
+
+resource "random_pet" "rg-name" {
+  prefix    = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  name      = random_pet.rg-name.id
+  location  = var.resource_group_location
+}
